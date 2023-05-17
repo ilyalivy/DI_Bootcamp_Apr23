dc283fcb3646eb87c59ee4f5f25edb59bb8eb025
ilyalivy <128998085+ilyalivy@users.noreply.github.com>
2023-05-17 21:13:08 +0300

    Week3 Day4 commit

---
+++ Week3/Day4/DailyChallenge/main.py
@@ -0,0 +1,88 @@
+# Instructions :
+#
+# The goal of the exercise is to create a class that will help you analyze a specific text. A text can be just a simple string, like “Today, is a happy day” or it can be an external text file.
+#
+#
+#
+# Part I
+#
+# First, we will analyze a simple string, like “A good book would sometimes cost as much as a good house.”
+#
+# Create a class called Text that takes a string as an argument and store the text in a attribute.
+# Hint: You need to manually copy-paste the text, straight into the code
+#
+# Implement the following methods:
+# a method to return the frequency of a word in the text (assume words are separated by whitespace) return None or a meaningful message.
+# a method that returns the most common word in the text.
+# a method that returns a list of all the unique words in the text.
+
+# Part II
+#
+# Then, we will analyze a text coming from an external text file. Download the_stranger.txt file.
+#
+# Implement a classmethod that returns a Text instance but with a text file:
+#
+#     >>> Text.from_file('the_stranger.txt')
+# Hint: You need to open and read the text from the text file.
+#
+#
+# Now, use the provided the_stranger.txt file and try using the class you created above.
+
+class Text:
+    def __init__(self, a):
+        self.a = a
+        self.text_lower = self.a.lower()
+        self.text_list = self.text_lower.split()
+
+    def word_frequency(self, word):
+        if word not in self.text_list:
+            return None
+        else:
+            return f"The word '{word}' appears {self.text_list.count(word)} times in the text."
+
+    def most_common_word(self):
+        word_count = {}
+        for word in self.text_list:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+        max_count = max(word_count.values())
+        return ', '.join([word for word, count in word_count.items() if count == max_count])
+
+    def most_unique_words(self):
+        word_count = {}
+        unique_words = []
+        for word in self.text_list:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+        for key, value in word_count.items():
+            if value == 1:
+                unique_words.append(key)
+        return unique_words
+
+
+
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename, 'r') as file:
+            text = file.read()
+        return cls(text)
+
+text_from_file = Text.from_file('the_stranger.txt')
+
+print(text_from_file.word_frequency('the'))
+print(text_from_file.most_common_word())
+print(text_from_file.most_unique_words())
+
+
+
+
+text_obj = Text('A good book would sometimes cost as much as a good house')
+
+print(text_obj.word_frequency('house'))
+print(text_obj.most_common_word())
+print(text_obj.most_unique_words())
---
+++ Week3/Day4/ExerciseXP/main.py
@@ -0,0 +1,61 @@
+# Exercise 1 – Random Sentence Generator
+#
+# Instructions
+#
+# Description: In this exercise we will create a random sentence generator. We will do this by asking the user how long the sentence should be and then printing the generated sentence.
+#
+# Hint : The generated sentences do not have to make sense.
+#
+# Download this word list
+#
+# Save it in your development directory.
+#
+# Create a function called get_words_from_file. This function should read the file’s content and return the words as a collection. What is the correct data type to store the words?
+#
+# Create another function called get_random_sentence which takes a single parameter called length. The length parameter will be used to determine how many words the sentence should have. The function should:
+# use the words list to get your random words.
+# the amount of words should be the value of the length parameter.
+#
+# Take the random words and create a sentence (using a python method), the sentence should be lower case.
+#
+# Create a function called main which will:
+#
+# Print a message explaining what the program does.
+#
+# Ask the user how long they want the sentence to be. Acceptable values are: an integer between 2 and 20. Validate your data and test your validation!
+# If the user inputs incorrect data, print an error message and end the program.
+# If the user inputs correct data, run your code.
+
+import random
+
+def get_words_from_file():
+    with open('sowpods.txt', 'r') as file:
+        words = file.read().splitlines()
+    return words
+
+# print(get_words_from_file())
+
+def get_random_sentence(length):
+    words = get_words_from_file()
+    sentence = ' '.join(random.choices(words, k = length)).lower()
+    return sentence
+
+# print(get_random_sentence(10))
+
+def main():
+    print('This program generates random sentences.')
+    while True:
+        length = input('How long should the sentence be (between 2 and 20)? ')
+        try:
+            length = int(length)
+            if length < 2 or length > 20:
+                print('Error: Please enter an integer between 2 and 20.')
+                continue
+            break
+        except ValueError:
+            print('Error: Please enter an integer.')
+
+    sentence = get_random_sentence(length)
+    return sentence
+
+print(main())
