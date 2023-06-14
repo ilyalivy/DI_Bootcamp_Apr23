627b184df3c95a66cd4399408928af9f3f6c3151
ilyalivy <128998085+ilyalivy@users.noreply.github.com>
2023-06-14 18:57:15 +0300

    Week6 Day7 Commit

---
+++ Week6/DAY7/XP:DC/manage_top/manage_app/serializers.py
@@ -0,0 +1,30 @@
+#mserializers.py
+from rest_framework import serializers
+from .models import *
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='department_detail')
+
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='employee_detail')
+
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+class TaskSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='task_detail')
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = '__all__'
